c6edb9b465c4fe8f23c7113e7261fe2ba758488a
jjuni1115 <junyege@naver.com>
2025-03-18 18:04:38 +0900

    feat: 회원가입 기능  - 회원가입 추가 정보 작업 진행 중

--- src/routes/pages/Register.jsx
+++ src/routes/pages/Register.jsx
@@ -1,19 +1,25 @@
 import {useNavigate, useNavigationType, useParams, useSearchParams} from "react-router-dom";
-import {Button, Card, Container, Form} from "react-bootstrap";
+import {Button, ButtonGroup, Card, Container, Form, ToggleButton} from "react-bootstrap";
 import useInput from "../../hook/useInput.jsx";
 import api from "../../util/axios.js";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const Register = () => {
 
 
     const [searchParam, setSearchParam] = useSearchParams();
 
-    const userEmail = useInput(searchParam.get("email"));
-    const userName = useInput(searchParam.get("name"));
+    const userEmail = useInput(searchParam.get("email") ?? "");
+    const userName = useInput(searchParam.get("name") ?? "");
+
+    const [step,setStep] = useState(1);
+
+    const [radioValue, setRadioValue] = useState('1');
 
 
     const userPassword = useInput("");
+
+    const grade = ["최하","하","중","중상","상","최상"];
 
     const navigate = useNavigate();
     const navigationType = useNavigationType();
@@ -37,6 +43,14 @@ const Register = () => {
 
     }
 
+    const nextStep = () => {
+        if(step === 1){
+            setStep(current => current + 1);
+        }else if (step === 2){
+            console.log("register");
+        }
+    }
+
     return (
         <Container className="d-flex flex-column align-items-center vh-100">
             <Card className="p-4  mt-5" style={{width: "350px"}}>
@@ -46,6 +60,8 @@ const Register = () => {
                 <p className="text-center">즐거운 농구 시작하기</p>
 
                 <hr className="text-light"/>
+
+                {step === 1 && (
 
                 <Form>
                     <Form.Group className="mb-2">
@@ -64,14 +80,44 @@ const Register = () => {
                         <Form.Control type="text" placeholder="닉네임" name="nickName"/>
                     </Form.Group>
                     <Form.Group className="mb-2">
-                        <Form.Check inline type="radio" label="남" name="gender"/>
-                        <Form.Check inline type="radio" label="여" name="gender"/>
+                        <Form.Check inline type="radio" label="남" name="gender" value="M"/>
+                        <Form.Check inline type="radio" label="여" name="gender" value="F"/>
                     </Form.Group>
-                    <Button variant="primary" className="w-100" onClick={registerUser}>
-                        가입
-                    </Button>
+
                 </Form>
+                    )}
+                {step === 2 && (
+                    <>
+                    <p className="text-center">농구 실력을 선택해주세요</p>
+
+                    <ButtonGroup className="mb-2">
+
+                        {
+                            grade.map((item,index) => (
+                                <ToggleButton
+                                    key={index}
+                                    id={index}
+                                    type="radio"
+                                    variant="outline-primary"
+                                    value={item}
+                                    onChange={(e) => setRadioValue(e.currentTarget.value)}
+                                    checked={radioValue === item}
+                                >
+                                    {item}
+                                </ToggleButton>
+                            ))
+                        }
+
+                    </ButtonGroup>
+                    </>
+                )}
+
+                <Button variant="primary" className="w-100" onClick={nextStep}>
+                    가입
+                </Button>
             </Card>
+
+
 
             <p className="mt-3 text-white">앱을 다운로드하세요.</p>
         </Container>
